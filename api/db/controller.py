b5340bfdc8cf01bba46e6fadd713dad00646fc3d
josh <itzdi0de@gmail.com>
2023-09-20 02:04:56 -0400

    win algo
    lets get some exp flowing

--- api/db/controller.py
+++ api/db/controller.py
@@ -6,6 +6,8 @@ def create_stats(db: Session, stats: str):
     #Check if server record exist already (Not much unique data to go by here so we filter on name and player host)
     server = db.query(models.Server).filter(models.Server.serverName == stats["serverName"]).filter(models.Server.hostPlayer == stats["hostPlayer"]).first()
 
+    winners = getWinner(stats)
+
     if not server:
         #Create server record
         server = models.Server(serverName=stats["serverName"], 
@@ -31,14 +33,26 @@ def create_stats(db: Session, stats: str):
 
     #Iterate over players in match and create records for them. Create a new record for each game event as player records are unique. For some reason player match data is stored here in the form of team id?
     for playerData in stats["players"]:
-        player = models.Player(playerName=playerData["name"],
+        if playerData["uid"] in winners:
+            player = models.Player(playerName=playerData["name"],
                                clientName=playerData["clientName"],
                                serviceTag=playerData["serviceTag"],
                                playerIp=playerData["ip"],
                                team=playerData["team"],
                                playerIndex=playerData["playerIndex"],
                                playerUID=playerData["uid"],
-                               primaryColor=playerData["primaryColor"])
+                               primaryColor=playerData["primaryColor"],
+                               playerExp=1)
+            
+        else:
+            player = models.Player(playerName=playerData["name"],
+                                clientName=playerData["clientName"],
+                                serviceTag=playerData["serviceTag"],
+                                playerIp=playerData["ip"],
+                                team=playerData["team"],
+                                playerIndex=playerData["playerIndex"],
+                                playerUID=playerData["uid"],
+                                primaryColor=playerData["primaryColor"])
             
         db.add(player)
         db.commit()
@@ -98,3 +112,116 @@ def create_stats(db: Session, stats: str):
             db.commit()
 
     return True
+
+
+#Return should be list of players as winners
+def getWinner(gameData):
+
+    #Get Slayer Winner
+    if gameData["game"]["variantType"] == "slayer":
+        if gameData["game"]["teamGame"]:
+            
+            #Find all teams in our game and sum their player scores
+            teams = {}
+            for player in gameData["players"]:
+                if player["team"] not in teams:
+                    teams[player["team"]] = player["playerGameStats"]["kills"]
+
+                else:
+                    teams[player["team"]] = player["playerGameStats"]["kills"] + teams[player["team"]]
+
+            win_team = max(teams, key=teams.get)
+
+            win_players = []
+            for player in gameData["players"]:
+                if player["team"] == win_team:
+                    win_players.append(player["uid"])
+
+            return win_players
+
+        else:
+            kills = 0
+            winner = []
+            for player in gameData["players"]:
+                if player["playerGameStats"]["kills"] >= kills:
+                    kills = player["playerGameStats"]["kills"]
+                    winner.append(player["uid"])
+
+            return winner
+    
+    #Get CTF Winner
+    elif gameData["game"]["variantType"] == "ctf":
+        teams = {}
+        for player in gameData["players"]:
+            if player["team"] not in teams:
+                for medal in player["playerMedals"]:
+                    if medal["medalName"] == "flag_captured":
+                        teams[player["team"]] = medal["count"]
+
+            else:
+                for medal in player["playerMedals"]:
+                    if medal["medalName"] == "flag_captured":
+                        teams[player["team"]] = medal["count"] + teams[player["team"]]
+
+        win_team = max(teams, key=teams.get)
+
+        win_players = []
+        for player in gameData["players"]:
+            if player["team"] == win_team:
+                win_players.append(player["uid"])
+
+        return win_players
+
+    elif gameData["game"]["variantType"] == "infection":
+        #Iterate over players and get sum of zombies killed and humans infected
+        #We will average these values to get a infection "score". This incentivizes players while human and infected (Thanks Mtn)
+        perf_score = 0
+        winner = ""
+        for player in gameData["players"]:
+            zombs = player["otherStats"]["zombiesKilled"]
+            infects = player["otherStats"]["humansInfected"]
+            player_score = zombs + infects / 2
+
+            if player_score >= perf_score:
+                winner = (player["uid"])
+                perf_score = player_score
+
+        return [winner]
+    
+    elif gameData["game"]["variantType"] == "vip":
+        if gameData["game"]["teamGame"]:
+            #This one gonna be fun
+            return None
+        
+        else:
+            #This one gonna be fun
+            return None
+    
+    elif gameData["game"]["variantType"] == "koth":
+        if gameData["game"]["teamGame"]:
+            return None
+        
+        else:
+            #Iterate over players and find max timeControllingHill
+            return None
+    
+    elif gameData["game"]["variantType"] == "oddball":
+        if gameData["game"]["teamGame"]:
+            #Iterate over teams and find team sum max score
+            return None
+        
+        else:
+            #Iterate over players and find max score
+            return None
+ 
+    elif gameData["game"]["variantType"] == "territories":
+        if gameData["game"]["teamGame"]:
+            #Iterate over teams and find team sum max score
+            return None
+        
+        else:
+            #Iterate over players and find max score
+            return None
+        
+    else:
+        return ['Guardians']
